29aba166888f7f95008edc9a16030bfbf3f17d04
Yeong-Huns <156881254+Yeong-Huns@users.noreply.github.com>
2024-08-27 12:03:18 +0900

    update PageNotFound

--- src/components/PageNotFound.vue
+++ src/components/PageNotFound.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>
-    <h3>Page Not Found</h3>
-  </div>
+    <div>
+        <h3>Page Not Found</h3>
+    </div>
 </template>
 
 <script>
-export default {};
+export default {
+    beforeMount() {
+        const path = this.$route.params.path;
+        console.log(`라우터에서 받은 params : ${path}`);
+    },
+};
 </script>
 
 <style scoped></style>
