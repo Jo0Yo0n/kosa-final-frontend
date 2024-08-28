8b2e44914a7b6c23b8f1c4ad4575d9cd804b2d36
Yeong-Huns <156881254+Yeong-Huns@users.noreply.github.com>
2024-08-28 22:52:13 +0900

    feat TextEditor

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ],
+    transpileDependencies: ['vuetify'],
 
-  devServer: {
-    proxy: 'https://hesil.site',
-    port: 3000
-  }
-})
+    devServer: {
+        proxy: 'https://hesil.site',
+    },
+});
