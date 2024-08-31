3bc790c10f4c0bb5ba4c0f18f9b61860160ef4e3
Jo0Yo0n <dlwndbs10@gmail.com>
2024-09-01 01:35:37 +0900

    Add: ProjectMembers, MemberCard 컴포넌트 추가
    - ProjectInfo에 들어가는 ProjectMembers 컴포넌트 추가
    - MemberCard 컴포넌트 추가

--- src/components/project-detail/ProjectInfo.vue
+++ src/components/project-detail/ProjectInfo.vue
@@ -8,50 +8,24 @@
  * 2024-08-28        JooYoon       최초 생성
 -->
 <template>
-    <div>
+    <div class="pa-4">
         <leader-info :leader="project.leader" />
 
-        <v-divider />
+        <v-divider class="my-6" />
 
         <recruitment-status :recruitments="project.recruitments" />
 
-        <v-divider />
+        <v-divider class="my-6" />
 
         <project-description :description="project.description" />
 
-        <v-divider />
+        <v-divider class="my-6" />
 
         <tech-stack :projectTechStack="project.projectTechStack" />
 
-        <v-divider />
+        <v-divider class="my-6" />
 
-        <v-row>
-            <v-col cols="12">
-                <h2>참여 중인 멤버</h2>
-                <v-list>
-                    <v-list-item v-for="recruitment in project.recruitments" :key="recruitment.jobId">
-                        <v-list-item-content>
-                            <v-list-item-title>{{ recruitment.jobName }}</v-list-item-title>
-                            <v-list-item-group>
-                                <v-list-item v-for="member in recruitment.members" :key="member.memberId">
-                                    <v-list-item-avatar>
-                                        <v-img :src="member.memberImg"></v-img>
-                                    </v-list-item-avatar>
-                                    <v-list-item-content>
-                                        <v-list-item-title>{{ member.memberNickname }}</v-list-item-title>
-                                        <v-list-item-subtitle>
-                                            <v-chip v-for="tech in member.techStack" :key="tech" x-small class="mr-1">
-                                                {{ tech }}
-                                            </v-chip>
-                                        </v-list-item-subtitle>
-                                    </v-list-item-content>
-                                </v-list-item>
-                            </v-list-item-group>
-                        </v-list-item-content>
-                    </v-list-item>
-                </v-list>
-            </v-col>
-        </v-row>
+        <project-members :recruitments="project.recruitments" />
     </div>
 </template>
 
@@ -60,10 +34,11 @@ import LeaderInfo from '@/components/project-detail/LeaderInfo.vue';
 import RecruitmentStatus from '@/components/project-detail/RecruitmentStatus.vue';
 import ProjectDescription from '@/components/project-detail/ProjectDescription.vue';
 import TechStack from '@/components/project-detail/TechStack.vue';
+import ProjectMembers from '@/components/project-detail/ProjectMembers.vue';
 
 export default {
     name: 'ProjectInfo',
-    components: { TechStack, ProjectDescription, RecruitmentStatus, LeaderInfo },
+    components: { ProjectMembers, TechStack, ProjectDescription, RecruitmentStatus, LeaderInfo },
     props: {
         project: {
             type: Object,
---
+++ src/components/project-detail/ProjectMembers.vue
@@ -0,0 +1,39 @@
+<!--
+ * fileName       : ProjectMembers
+ * author         : JooYoon
+ * date           : 2024-09-01
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-09-01           JooYoon           최초 생성
+-->
+
+<template>
+    <v-row>
+        <v-col cols="12">
+            <h2 class="mb-4">참여 중인 멤버</h2>
+            <div v-for="recruitment in recruitments" :key="recruitment.jobId" class="mb-4">
+                <h3 class="subtitle-1 font-weight-bold mb-2 text-h6">{{ recruitment.jobName }}</h3>
+                <div class="d-flex flex-wrap">
+                    <member-card v-for="member in recruitment.members" :key="member.memberId" :member="member" />
+                </div>
+            </div>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+import MemberCard from '@/components/MemberCard.vue';
+
+export default {
+    name: 'ProjectMembers',
+    components: { MemberCard },
+    props: {
+        recruitments: {
+            type: Array,
+        },
+    },
+};
+</script>
+
+<style scoped></style>
