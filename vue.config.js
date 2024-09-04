96cc204aa8fe7be8bc417c02da3206a77cecc4e2
Yeong-Huns <156881254+Yeong-Huns@users.noreply.github.com>
2024-09-04 20:59:14 +0900

    feature/node_config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
     transpileDependencies: ['vuetify'],
     css: {
@@ -16,6 +16,10 @@ module.exports = defineConfig({
             // '/api' 로 시작하는 모든 요청을 target url 로 프록시
             '^/api': {
                 target: `${process.env.VUE_APP_API_URL}`,
+                changeOrigin: true,
+            },
+            '^/node-api': {
+                target: `${process.env.VUE_APP_NODE_API_URL}`,
                 changeOrigin: true,
             },
         },
