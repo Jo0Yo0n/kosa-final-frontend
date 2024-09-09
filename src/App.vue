0fcda1fbbe1c2981358eca54afe0b9b58cb1a539
YunBins <kig0632@naver.com>
2024-09-09 14:27:41 +0900

    Feat: one-to-on chatting
    not yet

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
     <v-app>
         <header-layout />
-        <v-main>
-            <content-layout style="width: 1200px; margin: 0 auto" />
+        <v-main style="width: 1200px; margin: 0 auto">
+            <content-layout />
         </v-main>
+        <div class="chat-button">
+            <ChatButton color="#A1887F">mdi-account</ChatButton>
+            <ChatButton color="#8D6E63">mdi-account-multiple</ChatButton>
+        </div>
         <footer-layout />
     </v-app>
 </template>
@@ -12,10 +16,12 @@
 import HeaderLayout from './common/HeaderLayout.vue';
 import FooterLayout from './common/FooterLayout.vue';
 import ContentLayout from './common/ContentLayout.vue';
+import ChatButton from '@/components/chat/ChatButton.vue';
 
 export default {
     name: 'App',
     components: {
+        ChatButton,
         HeaderLayout,
         ContentLayout,
         FooterLayout,
@@ -25,4 +31,11 @@ export default {
     }),
 };
 </script>
-<style scoped></style>
+<style scoped>
+.chat-button {
+    position: fixed;
+    bottom: 130px;
+    right: 50px;
+    z-index: 1;
+}
+</style>
--- src/components/MemberCard.vue
+++ src/components/MemberCard.vue
@@ -6,69 +6,81 @@
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 2024-09-01           JooYoon           최초 생성
+ * 2024-09-08           yunbin            채팅 요청 추가
 -->
 
 <template>
-    <v-card class="ma-2 member-card" outlined>
-        <v-card-text class="pa-3">
-            <div class="d-flex mb-3">
-                <v-avatar size="100" class="mr-3" tile rounded="xxl">
-                    <v-img :src="member.memberImg"></v-img>
-                </v-avatar>
-                <div class="d-flex flex-column justify-center flex-grow-1">
-                    <div class="d-flex align-center justify-center mb-1">
-                      <v-tooltip bottom>
-                        <template v-slot:activator="{ on, attrs }">
-                <span
-                    class="text-h5 font-weight-bold black--text text-truncate"
-                    v-bind="attrs"
-                    v-on="on"
-                    style="max-width: 100px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;"
-                >
-                  {{ member.memberNickname }}
-                </span>
-                        </template>
-                        <span>{{ member.memberNickname }}</span>
-                      </v-tooltip>
+    <div>
+        <v-card class="ma-2 member-card" outlined rounded="lg" elevation="1" style="width: 300px">
+            <v-card-text class="pa-3">
+                <div class="d-flex mb-3">
+                    <v-avatar size="100" class="mr-3" tile rounded="xxl">
+                        <v-img :src="member.memberImg"></v-img>
+                    </v-avatar>
+                    <div class="d-flex flex-column justify-center flex-grow-1">
+                        <div class="d-flex align-center justify-center mb-1">
+                            <v-tooltip bottom>
+                                <template v-slot:activator="{ on, attrs }">
+                                    <span
+                                        class="text-h5 font-weight-bold black--text text-truncate"
+                                        v-bind="attrs"
+                                        v-on="on"
+                                        style="max-width: 100px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis"
+                                    >
+                                        {{ member.memberNickname }}
+                                    </span>
+                                </template>
+                                <span>{{ member.memberNickname }}</span>
+                            </v-tooltip>
 
-                        <v-btn icon :href="member.memberGithub" target="_blank" color="black" small class="ml-1">
-                            <v-icon>mdi-github</v-icon>
-                        </v-btn>
-                    </div>
-                    <div class="d-flex justify-center">
-                        <v-btn color="brown" dark small class="font-weight-light" style="width: 60%">커피 챗</v-btn>
-                    </div>
-                </div>
-            </div>
-
-            <div class="mt-3">
-                <div class="caption black--text mb-1 grey--text">진행 중인 프로젝트: {{ member.ongoingProjectCount }}개</div>
-                <div class="caption black--text mb-2 grey--text">완료한 프로젝트: {{ member.completedProjectCount }}개</div>
-                <div class="caption font-weight-bold black--text mb-1 text-h6">기술 스택</div>
-                <div class="d-flex flex-wrap">
-                    <template v-for="tech in member.techStack">
-                        <div v-if="tech.imgUrl" :key="tech.name" class="ma-1 tech-item">
-                            <v-avatar size="30" class="mb-1" tile>
-                                <v-img :src="tech.imgUrl" :alt="tech.name"></v-img>
-                            </v-avatar>
-                            <div class="caption text-center black--text">#{{ tech.name }}</div>
+                            <v-btn icon :href="member.memberGithub" target="_blank" color="black" small class="ml-1">
+                                <v-icon>mdi-github</v-icon>
+                            </v-btn>
                         </div>
-                        <v-chip v-else :key="tech.name" class="ma-2" outlined> #{{ tech.name }} </v-chip>
-                    </template>
+                        <div class="d-flex justify-center">
+                            <v-btn color="brown" dark small class="font-weight-light" style="width: 60%" @click="showModal = true">커피 챗</v-btn>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </v-card-text>
-    </v-card>
+
+                <div class="mt-3">
+                    <div class="caption black--text mb-1 grey--text">진행 중인 프로젝트: {{ member.ongoingProjectCount }}개</div>
+                    <div class="caption black--text mb-2 grey--text">완료한 프로젝트: {{ member.completedProjectCount }}개</div>
+                    <div class="caption font-weight-bold black--text mb-1 text-h6">기술 스택</div>
+                    <div class="d-flex flex-wrap">
+                        <template v-for="tech in member.techStack">
+                            <div v-if="tech.imgUrl" :key="tech.name" class="ma-1 tech-item">
+                                <v-avatar size="30" class="mb-1" tile>
+                                    <v-img :src="tech.imgUrl" :alt="tech.name"></v-img>
+                                </v-avatar>
+                                <div class="caption text-center black--text">#{{ tech.name }}</div>
+                            </div>
+                            <v-chip v-else :key="tech.name" class="ma-2" outlined> #{{ tech.name }} </v-chip>
+                        </template>
+                    </div>
+                </div>
+            </v-card-text>
+        </v-card>
+        <ChatModal v-model="showModal" :member="member"></ChatModal>
+    </div>
 </template>
 
 <script>
+import ChatModal from '@/components/chat/ChatModal.vue';
+
 export default {
     name: 'MemberCard',
+    components: { ChatModal },
     props: {
         member: {
             type: Object,
             required: true,
         },
+    },
+    data() {
+        return {
+            showModal: false,
+        };
     },
 };
 </script>
