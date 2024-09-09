6175a80065d5bffda2387aa07639ad14673fd02c
JooYoon <dlwndbs10@gmail.com>
2024-09-09 16:46:00 +0900

    Fix: 회원 가입 시 기술 스택 추가 버그 수정
    - 기술 스택 추가 안되는 버그 수정

--- src/components/login/TechStackSelector.vue
+++ src/components/login/TechStackSelector.vue
@@ -19,7 +19,7 @@ export default {
             type: Array,
             default: () => [],
         },
-        knownTech: {
+        techOptions: {
             type: Object,
             default: () => ({}),
         },
