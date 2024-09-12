546ee1c288ea92188fbe265f94388361e8294803
JooYoon <dlwndbs10@gmail.com>
2024-09-12 16:41:34 +0900

    Add: vercel.json 추가

--- vue.config.js
+++ vue.config.js
@@ -26,17 +26,4 @@ module.exports = defineConfig({
             },
         },
     },
-    prodServer: {
-        proxy: {
-            // '/api' 로 시작하는 모든 요청을 target url 로 프록시
-            '^/api': {
-                target: `${process.env.VUE_APP_API_URL}`,
-                changeOrigin: true,
-            },
-            '^/node-api': {
-                target: `${process.env.VUE_APP_NODE_API_URL}`,
-                changeOrigin: true,
-            },
-        },
-    },
 });
